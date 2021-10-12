26b4007f909003ea083fc3f6fffe5b447a78cd98
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-12 12:22:25 +1100

    Implemented functionality for displaying information for single phonebook entry -ex3.3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
@@ -30,6 +31,16 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 app.get('/info', (request, response) => {
