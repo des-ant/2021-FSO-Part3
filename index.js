02c0c61dfa0f98a513b2d29377edd61eb7a46fff
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-12 12:36:08 +1100

    Implemented functionality to delete single phonebook entry using HTTP DELETE request to URL -ex3.4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const express = require('express');
 const app = express();
 
@@ -41,6 +41,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
 });
 
 app.get('/info', (request, response) => {
