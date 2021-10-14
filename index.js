c28c26d40fbcf9d5d92436c9fe7872da85e8e877
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-15 00:06:18 +1100

    Updated handling of api/persons/:id and info routes to use database -ex3.18

--- index.js
+++ index.js
@@ -21,33 +21,15 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 // Use Cors middleware to allow requests from all origins
 app.use(cors());
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
   }
-];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+  next(error);
+};
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -76,14 +58,16 @@ app.post('/api/persons', (request, response) => {
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -109,21 +93,13 @@ app.delete('/api/persons/:id', (request, response, next) => {
 });
 
 app.get('/info', (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`
-  );
+  Person.find({}).then(persons => {
+    response.send(
+      `<p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`
+    );
+  });
 });
-
-const errorHandler = (error, request, response, next) => {
-  console.log(error.message);
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  }
-
-  next(error);
-};
 
 app.use(errorHandler);
 
